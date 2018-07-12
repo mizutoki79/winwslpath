9e0bb77088b57eb42957913a5986982a2331fd49
mizutoki_ <a.p9xelic5@gmail.com>
2018-07-13 00:17:45 +0900

    show usage

--- winwslpath/Program.cs
+++ winwslpath/Program.cs
@@ -6,7 +6,24 @@ namespace winwslpath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Error.WriteLine($"args.Length = {args.Length}");
+            if(args.Length >0)
+            {
+                Console.Error.WriteLine($"args[0] = {args[0]}");
+            }
+            if (args.Length == 0 || args[0] == "/h" || args[0] == "-h")
+            {
+                Usage();
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("winwslpath usage:");
+            Console.WriteLine("\t/a\tforce result to absolute path format");
+            Console.WriteLine("\t/u\ttranslate from a Windows path to a WSL path (default)");
+            Console.WriteLine("\t/w\ttranslate from a WSL path to a Windows path");
+            Console.WriteLine("\t/m\ttranslate from a Windows path to a Windows path, with '/' instead of '\\'");
         }
     }
 }
