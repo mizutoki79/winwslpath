42d447fc4522670233fd1ad9f23c33894547582f
mizutoki_ <a.p9xelic5@gmail.com>
2018-07-13 00:22:54 +0900

    add a little bit usage information

--- winwslpath/Program.cs
+++ winwslpath/Program.cs
@@ -15,15 +15,17 @@ namespace winwslpath
             {
                 Usage();
             }
+
         }
 
         static void Usage()
         {
-            Console.WriteLine("winwslpath usage:");
+            Console.WriteLine("winwslpath usage:\n");
             Console.WriteLine("\t/a\tforce result to absolute path format");
             Console.WriteLine("\t/u\ttranslate from a Windows path to a WSL path (default)");
             Console.WriteLine("\t/w\ttranslate from a WSL path to a Windows path");
             Console.WriteLine("\t/m\ttranslate from a Windows path to a Windows path, with '/' instead of '\\'");
+            Console.WriteLine("\t/h\tdisplay usage information");
         }
     }
 }
