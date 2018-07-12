27110223df80dae3ba2bb1b175902380509ec51a
mizutoki_ <a.p9xelic5@gmail.com>
2018-07-13 01:23:46 +0900

    最初の条件分岐の流れを変更

--- winwslpath/Program.cs
+++ winwslpath/Program.cs
@@ -6,22 +6,31 @@ namespace winwslpath
     {
         static void Main(string[] args)
         {
-            Console.Error.WriteLine($"args.Length = {args.Length}");
-            if(args.Length >0)
+            var i = 0;
+            do
             {
-                Console.Error.WriteLine($"args[0] = {args[0]}");
-            }
-            if (args.Length == 0 || args[0] == "/h" || args[0] == "-h")
-            {
-                Usage();
-            }
-
+                var arg = args.Length > 0 ? args[i] : "/h";
+                if (arg.IndexOf('-') == 0) arg = "/" + arg.Substring(1, arg.Length - 1);
+                switch (arg)
+                {
+                    case "/h":
+                    case "/u":
+                        Usage();
+                        return;
+                    case "/w":
+                        return;
+                    case "/m":
+                        return;
+                    default:
+                        return;
+                }
+            } while (++i < args.Length);
         }
 
         static void Usage()
         {
             Console.WriteLine("winwslpath usage:\n");
-            Console.WriteLine("\t/a\tforce result to absolute path format");
+            Console.WriteLine("\t/a\tforce result to absolute path format (future use)");
             Console.WriteLine("\t/u\ttranslate from a Windows path to a WSL path (default)");
             Console.WriteLine("\t/w\ttranslate from a WSL path to a Windows path");
             Console.WriteLine("\t/m\ttranslate from a Windows path to a Windows path, with '/' instead of '\\'");
