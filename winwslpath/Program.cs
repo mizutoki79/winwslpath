b14c977d553f42c4b49873cf39c3d9f4cf5f8d7c
mizutoki_ <a.p9xelic5@gmail.com>
2018-08-17 21:22:27 +0900

    Enable to use `/a` 絶対パスで返せる様に

--- winwslpath/Program.cs
+++ winwslpath/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace winwslpath
 {
@@ -6,19 +8,30 @@ namespace winwslpath
     {
         static void Main(string[] args)
         {
-            var i = 0;
-            if (i < args.Length)
+            if (args.Length == 0)
             {
-                var arg = args.Length > 0 ? args[i] : "/h";
+                Usage();
+                return;
+            }
+            var i = 0;
+            var isAbsolute = args.Contains("-a") || args.Contains("/a");
+            while (i < args.Length)
+            {
+                var arg = args[i];
                 switch (arg)
                 {
+                    case "-a":
+                    case "/a":
+                        i++;
+                        continue;
                     case "-u":
                     case "/u":
                         // TODO: 分かりやすい Exception
-                        var path = args[++i];
-                        var newPath = path.Replace('\\', '/');
+                        var oldPath = args[++i];
+                        if (isAbsolute) oldPath = Path.GetFullPath(oldPath);
+                        var newPath = oldPath.Replace('\\', '/');
                         Console.WriteLine(newPath);
-                        break;
+                        return;
                     case "-w":
                     case "/w":
                     case "-m":
@@ -35,7 +48,7 @@ namespace winwslpath
         static void Usage()
         {
             Console.WriteLine("winwslpath usage:\n");
-            // Console.WriteLine("\t/a\tforce result to absolute path format");
+            Console.WriteLine("\t/a\tforce result to absolute path format");
             Console.WriteLine("\t/u\ttranslate from a Windows path to a WSL path (default)");
             // Console.WriteLine("\t/w\ttranslate from a WSL path to a Windows path");
             // Console.WriteLine("\t/m\ttranslate from a Windows path to a Windows path, with '/' instead of '\\'");
